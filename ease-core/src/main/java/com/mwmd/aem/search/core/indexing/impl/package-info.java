b947edde5c7ecf43c9eebab83996a185a1b2b765
Matthias Wermund <matthias@wermund.de>
2013-08-24 21:21:04 -0700

    Added package javadoc

--- ease-core/src/main/java/com/mwmd/aem/search/core/indexing/impl/package-info.java
+++ ease-core/src/main/java/com/mwmd/aem/search/core/indexing/impl/package-info.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Core implementation of EASE. Provides the central {@link IndexServiceImpl} and various listeners. Queue maintenance
  * and index data transfer happens asynchronously, which is why this package contains a number of Runnable 
  * implementations.
