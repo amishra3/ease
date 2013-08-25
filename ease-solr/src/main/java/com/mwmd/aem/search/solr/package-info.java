d8a17ecdf62779b8abed85a64549c7779e2027f1
Matthias Wermund <matthias@wermund.de>
2013-08-24 21:12:40 -0700

    Add package javadoc

---
+++ ease-solr/src/main/java/com/mwmd/aem/search/solr/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Apache Solr connector implementation for the EASE framework (<b>E</b>xternal <b>A</b>EM <b>S</b>earch
+ * <b>E</b>xtension). The EASE framework provides a technology-agnostic way of integrating external search platforms
+ * with Adobe Experience Manager.
+ */
+package com.mwmd.aem.search.solr;
